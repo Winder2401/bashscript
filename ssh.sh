b0167dc57b3eb2003b7579d966c3deb21aeded8d
Winder <108935241+winder2412@users.noreply.github.com>
2023-03-17 19:41:24 +0330

    Delete ssh.sh

--- ssh.sh
+++
@@ -1,6 +0,0 @@
-##ssh domain change
-curl https://get.acme.sh | sh
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m xxxx@xxxx.com
-~/.acme.sh/acme.sh --issue -d ad.winder.store --standalone
-~/.acme.sh/acme.sh --installcert -d ad.winder.store --key-file /root/private.key --fullchain-file /root/cert.crt
